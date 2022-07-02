df7db76d74fb51966fd4887f44408b75119f6c72
Vadim <vadik.kortkov@yandex.ru>
2022-07-02 19:15:21 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import {
 import { Main } from './components/Main/Main'
 import { NewsPage } from './components/NewsPage/NewsPage';
 import { useSelector } from 'react-redux'
-import { ProtectedRouter } from './components/ProtectedRouter/ProtectedRouter';
+/* import { ProtectedRouter } from './components/ProtectedRouter/ProtectedRouter'; */
 
 const App = (() => {
   const login = useSelector(state => state.login)
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={login
+        {/*  <Route path="/login" element={login
         ?<Navigate to='/news' />
         :<Login />} />
         <Route path="/news" element={
@@ -24,7 +24,20 @@ const App = (() => {
           </ProtectedRouter>
         }/>
         <Route path={`/news/:id`} element={<NewsPage />} />
-        <Route path="*" element={<Navigate to='/news' />} />
+        <Route path="*" element={<Navigate to='/news' />} /> */}
+        {login
+          ?
+          <>
+            <Route path={`/news/:id`} element={<NewsPage />} />
+            <Route path='/news' element={<Main />} />
+            <Route path="*" element={<Navigate to='/news' />} />
+          </>
+          :
+          <> 
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to='/login' />} />
+          </>
+        }
       </Routes>
     </div>
   );
