96c4f7df82fa5406f5e4197222c4937482507510
Vadim <vadik.kortkov@yandex.ru>
2022-07-02 18:50:11 +0300

    paginat

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,52 +7,64 @@ import {
 } from "react-router-dom";
 import { ActionCard } from '../UI/Card/Card';
 import { Btn } from '../UI/Button/Button';
+import { usePagination } from "react-use-pagination";
+import { PaginationItem } from '@mui/material';
 export const Main = ((props) => {
     const [uncos, setUncos] = useState([])
-    const [last, setLast] = useState(10)
-    const [first, setFirst] = useState(0)
-    const [message, setMessage] = useState('')
-    const [disable, setDisable] = useState(false)
     const navigate = useNavigate()
     const getNews = async () => {
         let response = await news()
         setUncos(response.data.news.concat(response.data.news))
     }
-    useEffect(() => {
-        getNews()
-    }, [])
-
-    function moreNews() {
-        if (last > uncos.length) {
-            setMessage('На данный момент это все новости')
-            setDisable(true)
-        } else {
-            setLast((curent) => curent + 10)
-            setFirst((curent) => curent + 10)
-        }
-
-    }
+    const {
+        currentPage,
+        totalPages,
+        setNextPage,
+        setPreviousPage,
+        nextEnabled,
+        previousEnabled,
+        startIndex,
+        endIndex,
+        setPage,
+    } = usePagination({ totalItems: uncos.length, initialPageSize: 10 });
+    
     function showNews(item) {
         navigate(`/news/${item.id}`)
     }
-    console.log(last, uncos.length)
+
+    useEffect(() => {
+        getNews()
+    }, [])
+   
     return (
         <div className="main">
             <Header />
-            {uncos
-                ? <div className="main__content">
+            <div>
+                <div className="main__content">
                     <div className="main__container">
-                        {uncos.slice(first, last).map((card) => (
+                        {uncos.slice(startIndex, endIndex + 1).map((card) => (
                             <ActionCard
                                 showNews={showNews}
                                 card={card}
                                 key={card.id} />
                         ))}
                     </div>
-                    <Btn disable={disable} type="button" onClick={moreNews} text="Больше новостей" />
-                    <h1>{message}</h1>
+                    <div className="main__buttons">
+                        <Btn onClick={setPreviousPage} disabled={!previousEnabled} text="Предыдущая страница" />
+                        {[...Array(totalPages).keys()].map((el) => (
+                            <PaginationItem
+                                onClick={() => setPage(el)}
+                                key={el}
+                                page={el+1}
+                                variant="outlined"
+                                disabled={el===currentPage?true:false}
+                                selected={el===currentPage?true:false}
+                            />
+                        ))}
+                        <Btn onClick={setNextPage} disabled={!nextEnabled} text="Следующая страница" />
+                    </div>
                 </div>
-                : ''}
+            </div>
         </div>
     )
 }) 
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button';
 export const Btn = (props) => {
     return(
-        <Button style={props.disable?{display:'none'}:{display:'block'}} className='button' variant="contained" size="medium" type={props.type} onClick={props.onClick}>{props.text}</Button> 
+        <Button disabled={props.disabled} className='button' variant="contained" size="medium" type={props.type} onClick={props.onClick}>{props.text}</Button> 
     )
 }
